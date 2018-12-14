1254ada5ccaf1bc1eeed5628a221c220025e4774
HeinHtetAung <heinhtetaung.itlife@gmail.com>
2018-12-14 17:07:52 +0630

    version upgrade again and solved error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import {Switch,Route,BrowserRouter,Link} from 'react-router-dom';
+import {Switch,Route,BrowserRouter} from 'react-router-dom';
 import Home from './views/Home.js';
 import Register from './views/Register.js';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        hello
-      </div>
+    	<div>
+    		<div>
+    			<h2>Header</h2>
+    			<hr/>
+    		</div>
+    		<div>
+    			<br />
+	    		<BrowserRouter>
+	    	   		<Switch>
+	            		<Route exact path="/" component={Home} />
+	            		<Route path="/register" component={Register} />         
+	          		</Switch>
+	        	</BrowserRouter>
+	        	<br />
+	        </div>
+	        <div>
+	        	<hr />
+	        	<h2>Footer</h2>
+	        </div>
+    	</div>
     );
   }
 }
