df34914db378e01794cbbf1b63577aa43190beed
HeinHtetAung <heinhtetaung.itlife@gmail.com>
2018-12-17 17:07:01 +0630

    register ui finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {Switch,Route,BrowserRouter} from 'react-router-dom';
+import {Switch,Route,BrowserRouter, Link} from 'react-router-dom';
 import Home from './views/Home.js';
 import Register from './views/Register.js';
 import './scss/bootstrapcustom.scss'; 
@@ -9,35 +9,34 @@ import './scss/bootstrapcustom.scss';
 class App extends Component {
   render() {
     return (
+		<BrowserRouter>
 			<div>
 				<nav className="navbar sticky-top navbar-light bg-grey borderbottom navback">
 					<div className="container">
-						<h4 class="my-0 mr-md-auto font-weight-normal">{'{}'} DevPlanet</h4>
-						<nav class="my-2 my-md-0 mr-md-3">
-							<a class="p-2 text-dark" href="#">Questions</a>
-							<a class="p-2 text-dark" href="#">Articles</a>
-							<a class="p-2 text-dark" href="#">Jobs</a>
-							<a class="p-2 text-dark" href="#">Advertising</a>
-						</nav>			
-						<a class="btn btn-outline-primary" href="#">Sign up</a>		
+						<h4 className="my-0 mr-md-auto font-weight-normal"><Link to='/'>{'{}'} DevPlanet</Link></h4>
+						<nav className="my-2 my-md-0 mr-md-3">
+							<a className="p-2 text-dark" href="#">Questions</a>
+							<a className="p-2 text-dark" href="#">Articles</a>
+							<a className="p-2 text-dark" href="#">Jobs</a>
+							<a className="p-2 text-dark" href="#">Advertising</a>
+						</nav>		
+						<Link className="btn btn-outline-primary" to='/register'>Sign up</Link>
 					</div>
 				</nav>
 				<div className="container">													
-					<br />
-					<BrowserRouter>
-						<Switch>
-								<Route exact path="/" component={Home} />
-								<Route path="/register" component={Register} />         
-							</Switch>
-					</BrowserRouter>
-					<br />
-					
+					<br/>					
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/register" component={Register} />         
+					</Switch>					
+					<br/>					
 				</div>
-				<footer class="container">
-					<p class="float-right"><a href="#">Back to top</a></p>
+				<footer className="container">
+					<p className="float-right"><a href="#">Back to top</a></p>
 					<p>© 2017-2018 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
 				</footer>
 			</div>
+			</BrowserRouter>
     );
   }
 }
--- frontend/src/views/Register.js
+++ frontend/src/views/Register.js
@@ -3,14 +3,29 @@ import React, { Component } from 'react';
 class Register extends Component {
   render() {
     return (
-      <div>
-      <div>
-        Register !!!.
-      </div>
       <div className="container">
-        
-      </div>
-      </div>
+        <form>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Username</label>
+            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Full Name" />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+            {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          </div>
+          {/* <div className="form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" for="exampleCheck1">Check me out</label>
+          </div> */}
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+
+      </div>      
     );
   }
 }
