1b440c28bb24e4ba5441c18a5cfc329587f56c69
HeinHtetAung <heinhtetaung.itlife@gmail.com>
2018-12-18 14:26:40 +0630

    fix indent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,33 +9,33 @@ import './scss/custom.scss';
 class App extends Component {
   render() {
     return (
-		<BrowserRouter>
-			<div>
-				<nav className="navbar sticky-top navbar-light bg-grey borderbottom navback">
-					<div className="container">
-						<h4 className="my-0 mr-md-auto font-weight-normal"><Link to='/'>{'{}'} DevPlanet</Link></h4>
-						<nav className="my-2 my-md-0 mr-md-3">
-							<a className="p-2 text-dark" href="#">Questions</a>
-							<a className="p-2 text-dark" href="#">Articles</a>
-							<a className="p-2 text-dark" href="#">Jobs</a>
-							<a className="p-2 text-dark" href="#">Advertising</a>
-						</nav>		
-						<Link className="btn btn-outline-primary" to='/register'>Sign up</Link>
+			<BrowserRouter>
+				<div>
+					<nav className="navbar sticky-top navbar-light bg-grey borderbottom navback">
+						<div className="container">
+							<h4 className="my-0 mr-md-auto font-weight-normal"><Link to='/'>{'{}'} DevPlanet</Link></h4>
+							<nav className="my-2 my-md-0 mr-md-3">
+								<a className="p-2 text-dark" href="#">Questions</a>
+								<a className="p-2 text-dark" href="#">Articles</a>
+								<a className="p-2 text-dark" href="#">Jobs</a>
+								<a className="p-2 text-dark" href="#">Advertising</a>
+							</nav>		
+							<Link className="btn btn-outline-primary" to='/register'>Sign up</Link>
+						</div>
+					</nav>
+					<div className="container">													
+						<br/>					
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route path="/register" component={Register} />         
+						</Switch>					
+						<br/>					
 					</div>
-				</nav>
-				<div className="container">													
-					<br/>					
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route path="/register" component={Register} />         
-					</Switch>					
-					<br/>					
+					<footer className="container">
+						<p className="float-right"><a href="#">Back to top</a></p>
+						<p>© 2017-2018 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+					</footer>
 				</div>
-				<footer className="container">
-					<p className="float-right"><a href="#">Back to top</a></p>
-					<p>© 2017-2018 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
-				</footer>
-			</div>
 			</BrowserRouter>
     );
   }
